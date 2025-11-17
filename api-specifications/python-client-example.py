bef20600849f40f7defba277b0085ce21fefb7a6
F. H. Campbell <fhcampbell@mosaezorg.com>
2025-11-17 23:39:54 +0100

    Update python-client-example.py

--- api-specifications/python-client-example.py
+++ api-specifications/python-client-example.py
@@ -1,1 +1,10 @@
+"""
+MosaeQ Platform Python Client Example
+"""
 
+import requests
+import json
+
+class MosaeQClient:
+    def __init__(self, api_key, base_url="https://api.mosaeq.com/v1"):
+# ... (rest of the Python code from my file)
